97f3b95730c5df751cb3d5dd6560ad84f3614cbf
Andrey Zaytsev <azaytsev@aligntech.com>
2016-03-01 15:14:28 +0300

    Green test for simple template processing

--- src/main/java/com/github/zstd/opencms/thymeleaf/OpenCmsContext.java
+++ src/main/java/com/github/zstd/opencms/thymeleaf/OpenCmsContext.java
@@ -4,14 +4,24 @@ import org.thymeleaf.context.IContext;
 import org.thymeleaf.context.VariablesMap;
 
 import java.util.Locale;
+import java.util.Map;
 
 public class OpenCmsContext implements IContext {
+
+    private VariablesMap<String, Object> map;
+
+    public OpenCmsContext(Map<String,Object> params) {
+        map = new VariablesMap<>();
+        map.putAll(params);
+    }
+
     public VariablesMap<String, Object> getVariables() {
-        return new VariablesMap<String, Object>();
+        return map;
     }
 
     public Locale getLocale() {
-        return null;
+        // TODO proper locale handling
+        return new Locale("EN");
     }
 
     public void addContextExecutionInfo(String s) {
---
+++ src/test/java/com/github/zstd/opencms/thymeleaf/TemplatingTest.java
@@ -0,0 +1,91 @@
+package com.github.zstd.opencms.thymeleaf;
+
+import com.google.gwt.thirdparty.guava.common.collect.ImmutableMap;
+import com.google.gwt.thirdparty.guava.common.io.ByteStreams;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opencms.file.CmsFile;
+import org.thymeleaf.TemplateEngine;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class TemplatingTest {
+
+    private CmsObjectAdapter cmsObjectAdapter;
+
+    @Before
+    public void setUp() throws Exception {
+        cmsObjectAdapter = Mockito.mock(CmsObjectAdapter.class);
+    }
+
+    @Test
+    public void testSimple() throws Exception {
+        String templateName = "item";
+        givenTemplateExists(templateName,getResourceAsBytes("/templates/item.html"));
+
+        Map<String,Object> params = ImmutableMap.<String,Object>of("title","The Title");
+        String result = whenGenerateFromTemplate(cmsObjectAdapter, templateName, params);
+
+        assertEquals("<div>The Title</div>",result);
+    }
+
+    private byte[] getResourceAsBytes(String name) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        try(InputStream is = this.getClass().getResourceAsStream(name)) {
+            ByteStreams.copy(is,result);
+        }
+        return result.toByteArray();
+    }
+
+    private void givenTemplateExists(String templateName, byte[] templateContent) {
+        Mockito.when(cmsObjectAdapter.existsResource(templateName)).thenReturn(true);
+        Mockito.when(cmsObjectAdapter.readFile(templateName)).thenReturn(new StubCmsFile(templateContent));
+    }
+
+    public static String whenGenerateFromTemplate(CmsObjectAdapter cmso,
+                                                  String templateName,
+                                                  Map<String,Object> params){
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(new VfsTemplateResolver(cmso));
+        OpenCmsContext context = new OpenCmsContext(params);
+        return templateEngine.process(templateName, context);
+    }
+
+    private static class StubCmsFile extends CmsFile {
+
+        private byte[] content;
+
+        public StubCmsFile(byte[] content) {
+            super(null,
+            null,
+            null,
+            0,
+            0,
+            null,
+            null,
+            0L,
+            null,
+            0,
+            null,
+            0L,
+            0L,
+            0,
+            0,
+            0L,
+            0,
+            content);
+            this.content = content;
+        }
+
+        @Override
+        public byte[] getContents() {
+            return content;
+        }
+    }
+}
